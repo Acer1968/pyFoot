291f78f3a4b6e23ad3870aebb5bb6ee3fb63e8c7
Joshua Gohlke <jocago@gmail.com>
2017-02-26 10:34:24 -0500

    Added 'All' filtering of get_element

--- tools/data.py
+++ tools/data.py
@@ -4,6 +4,7 @@ import datetime
 path = ''
 file = 'save.sqlite'
 
+
 def make_new_db():
     import os
     from data.scripts import db_tables_populate_scripts, db_tables_create_scripts
@@ -19,3 +20,38 @@ def make_new_db():
     conn.close()
 
 
+def is_game():
+    isgame = True
+    import os.path
+    if os.path.isfile(path+file):
+        player_name, current_date = get_element(['player_name','current_date'])
+        if player_name is None or current_date is None:
+            isgame = False
+    else:
+        isgame = False
+    return isgame
+
+
+def get_element(*wargs):
+    try:
+        conn = sqlite.connect(path + file)
+        resp = []
+        if wargs[0].lower() == 'all':
+            sql = "SELECT e_value from Game"
+            cursor = conn.execute(sql)
+            r = None
+            for row in cursor:
+                r = row[0]
+            resp.append(r)
+        else:
+            for name in wargs:
+                sql = "SELECT e_value from Game WHERE e_name = %s" % str(name)
+                cursor = conn.execute(sql)
+                r = None
+                for row in cursor:
+                    r = row[0]
+                resp.append(r)
+        conn.close()
+    except Exception as e:
+        raise e
+    return resp
