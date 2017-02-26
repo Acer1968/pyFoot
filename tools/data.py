5fe25fcfd2380227a715f3599d0fcf84563e9157
Joshua Gohlke <jocago@gmail.com>
2017-02-25 23:00:03 -0500

    Moved intitial scripts to data.scripts.py

--- tools/data.py
+++ tools/data.py
@@ -1,21 +1,21 @@
 import sqlite3 as sqlite
+import datetime
 
 path = ''
 file = 'save.sqlite'
 
 def make_new_db():
     import os
+    from data.scripts import db_tables_populate_scripts, db_tables_create_scripts
     try:
         os.remove(path+file)
     except Exception:
         pass
     conn = sqlite.connect(path + file)
-    sql = '''CREATE TABLE COMPANY
-           (ID      INT         PRIMARY KEY     NOT NULL,
-           NAME     TEXT                        NOT NULL,
-           AGE      INT                         NOT NULL,
-           ADDRESS  CHAR(50),
-           SALARY   REAL);
-           '''
-    conn.execute(sql)
+    for table in db_tables_create_scripts:  # Create the tables
+        conn.execute(db_tables_create_scripts[table])
+    for table in db_tables_populate_scripts:  # Populate the new tables
+        conn.execute(db_tables_populate_scripts[table])
     conn.close()
+
+
